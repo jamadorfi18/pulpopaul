fa4de1764e038c750f01677d81a2688d36a0cea8
Juan José González <juanjosegzl@gmail.com>
2017-06-22 19:51:12 -0500

    What's the best I can do in one hour?
    Adding basic models for teams and matches, I did not implement many to many relationship
    in models and I think that's needed
    
    Basic routes: no validations at all. take that!
    
    basic instructins on how to install dependencies of the app, basic cli client to create
    db and run other tasks

---
+++ manage.py
@@ -0,0 +1,18 @@
+from flask_script import Manager, Server
+from main import app, db, Match, Team
+
+manager = Manager(app)
+
+# Commands
+manager.add_command("server", Server())
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, Match=Match, Team=Team)
+
+if __name__ == "__main__":
+    manager.run()
