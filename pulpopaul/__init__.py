4f5597cfd9c9b8bd70fbf3afaeb18af7c573c5e8
Juan Jose Gonzalez <juan.gonzalez@adroll.com>
2017-06-23 20:20:34 -0500

    app factory

--- pulpopaul/__init__.py
+++ pulpopaul/__init__.py
@@ -6,19 +6,19 @@ from controllers.team import team_blueprint
 from controllers.match import match_blueprint
 from config import DevConfig
 
-# init app
-app = Flask(__name__)
-app.config.from_object(DevConfig)
-db.init_app(app)
+def create_app(object_name):
+    # init app
+    app = Flask(__name__)
+    app.config.from_object(object_name)
+    db.init_app(app)
 
-# register controller
-app.register_blueprint(team_blueprint)
-app.register_blueprint(match_blueprint)
+    # register controller
+    app.register_blueprint(team_blueprint)
+    app.register_blueprint(match_blueprint)
 
-# redirect to default view
-@app.route("/")
-def index():
-    return redirect(url_for('team.get_teams'))
+    # redirect to default view
+    @app.route("/")
+    def index():
+        return redirect(url_for('team.get_teams'))
 
-if __name__ == '__main__':
-    app.run()
+    return app
